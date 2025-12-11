3bfe45b8c81413278b16af68596daf8c13e58228
jakbif <vladimirinc4@gmail.com>
2025-12-11 20:53:26 +0300

    Update main.cpp
    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,1 +1,12 @@
-При выполнении задания своего варианта нужно вписать номер и название своего варианта, а также свои имя и фамилию.
+#include <iostream>
+#include "file_reader.h"
+using namespace std;
+int main() {
+    cout << "Roza vetrov" << endl;
+    cout << "Variant: 6" << endl;
+    cout << "Author: Kvit Ilya" << endl;
+    cout << "Group: PI-401" << endl;
+    auto records = readDataFromFile("data.txt");
+    cout << "\nRecords loaded: " << records.size() << endl;
+    return 0;
+}
